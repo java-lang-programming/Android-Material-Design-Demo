2125803ec3c0caed0fab325188cca7c730d29870
masayasuzuki <msuzuki@masayasuzuki-no-Mac-mini.local>
2016-11-13 20:15:06 +0900

    updated underline processing.

--- app/src/main/java/com/java_lang_programming/android_material_design_demo/ui/PreferenceDemoFragment.java
+++ app/src/main/java/com/java_lang_programming/android_material_design_demo/ui/PreferenceDemoFragment.java
@@ -1,8 +1,13 @@
 package com.java_lang_programming.android_material_design_demo.ui;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.java_lang_programming.android_material_design_demo.R;
 
@@ -17,6 +22,18 @@ public class PreferenceDemoFragment extends PreferenceFragmentCompat {
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         // Load the preferences from an XML resource
         setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+
+        // delete under line
+        setDivider(new ColorDrawable(Color.TRANSPARENT));
+        setDividerHeight(0);
+
+        return view;
     }
 
     @Override
